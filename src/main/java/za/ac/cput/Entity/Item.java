1835f989d2f4e15f5889090aeb0854df102e533c
Na'eem Mahoney <mahoneynaeem@gmail.com>
2021-06-07 11:00:47 +0200

    Created Branch

--- src/main/java/za/ac/cput/Entity/Item.java
+++ src/main/java/za/ac/cput/Entity/Item.java
@@ -1,4 +1,8 @@
 package za.ac.cput.Entity;
+//Na'eem Mahoney
+//218190751
+//ADP3
+//Assignment 3
 
 public class Item {
 }
--- src/main/java/za/ac/cput/Factory/ItemFactory.java
+++ src/main/java/za/ac/cput/Factory/ItemFactory.java
@@ -1,4 +1,8 @@
 package za.ac.cput.Factory;
+//Na'eem Mahoney
+//218190751
+//ADP3
+//Assignment 3
 
 public class ItemFactory {
 }
