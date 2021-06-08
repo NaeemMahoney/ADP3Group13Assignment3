1e6e0440cb719f445f0fc904162fe1908d3c5a68
Na'eem Mahoney <mahoneynaeem@gmail.com>
2021-06-08 19:45:00 +0200

    Factory complete for Item and PrescriptionItem entities

--- src/main/java/za/ac/cput/Factory/ItemFactory.java
+++ src/main/java/za/ac/cput/Factory/ItemFactory.java
@@ -3,6 +3,19 @@ package za.ac.cput.Factory;
 //218190751
 //ADP3
 //Assignment 3
+//Factory for Item Entity
+
+import za.ac.cput.Entity.Item;
 
 public class ItemFactory {
+    public static Item build(String itemName, double itemPrice, String itemType){
+        if(itemName.isEmpty() || itemPrice < 0 || itemType.isEmpty())
+            return null;
+        int randomNumber;
+        randomNumber = (int)Math.floor(Math.random()*(1000-1+1)+1);
+        String itemNumber = "ITM"+Integer.toString(randomNumber);
+        String prescriptionNumber = "PRM"+Integer.toString(randomNumber);
+        return new Item.Builder().itemNumber(itemNumber).prescriptionNumber(prescriptionNumber)
+                .itemName(itemName).itemPrice(itemPrice).itemType(itemType).builder();
+    }
 }
--- src/main/java/za/ac/cput/Factory/PrescriptionItemFactory.java
+++ src/main/java/za/ac/cput/Factory/PrescriptionItemFactory.java
@@ -1,4 +1,19 @@
 package za.ac.cput.Factory;
+//Na'eem Mahoney
+//218190751
+//ADP3
+//Assignment 3
+//Factory for PrecriptionItem Entity
+
+import za.ac.cput.Entity.PrescriptionItem;
 
 public class PrescriptionItemFactory {
+    public static PrescriptionItem build(){
+        int randomNumber;
+        randomNumber = (int)Math.floor(Math.random()*(1000-1+1)+1);
+        String itemNumber = "ITM"+Integer.toString(randomNumber);
+        String prescriptionNumber = "PRM"+Integer.toString(randomNumber);
+        return new PrescriptionItem.Builder().setPrescriptionNumber(prescriptionNumber).
+                setItemNumber(itemNumber).builder();
+    }
 }
