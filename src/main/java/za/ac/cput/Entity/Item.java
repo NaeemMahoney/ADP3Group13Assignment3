e67a3518dbb733af6d8136bd288e7e0b08381cd4
Na'eem Mahoney <mahoneynaeem@gmail.com>
2021-06-08 16:34:19 +0200

    Builder complete for Item and PrescriptionItem entities

--- src/main/java/za/ac/cput/Entity/Item.java
+++ src/main/java/za/ac/cput/Entity/Item.java
@@ -3,6 +3,63 @@ package za.ac.cput.Entity;
 //218190751
 //ADP3
 //Assignment 3
+//Builder Class for Item Entity
 
 public class Item {
+    private String itemNumber, prescriptionNumber, itemName, itemType;
+    private double itemPrice;
+
+    private Item (Builder builder){
+        this.itemNumber=builder.itemNumber;
+        this.prescriptionNumber=builder.prescriptionNumber;
+        this.itemName=builder.itemName;
+        this.itemPrice=builder.itemPrice;
+        this.itemType=builder.itemType;
+    }
+
+    public static class Builder{
+        private String itemNumber, prescriptionNumber, itemName, itemType;
+        private double itemPrice;
+
+        public Builder itemNumber(String itemNumber){
+            this.itemNumber = itemNumber;
+            return this;
+        }
+
+        public Builder prescriptionNumber(String prescriptionNumber){
+            this.prescriptionNumber = prescriptionNumber;
+            return this;
+        }
+
+        public Builder itemName(String itemName){
+            this.itemName = itemName;
+            return this;
+        }
+
+        public Builder itemPrice(double itemPrice){
+            this.itemPrice = itemPrice;
+            return this;
+        }
+
+        public Builder itemType(String itemType){
+            this.itemType = itemType;
+            return this;
+        }
+
+        public Item builder(){
+            return new Item(this);
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "itemNumber='" + itemNumber + '\'' +
+                ", prescriptionNumber='" + prescriptionNumber + '\'' +
+                ", itemName='" + itemName + '\'' +
+                ", itemType='" + itemType + '\'' +
+                ", itemPrice=" + itemPrice +
+                '}';
+    }
 }
