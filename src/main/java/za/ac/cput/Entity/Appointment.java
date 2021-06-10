8d1577a01490a8da3f6a5dea86c87833e6364de5
Nolusindiso Makosa <makosanolusindiso@gmail.com>
2021-06-10 21:25:42 +0200

    Finished appointment entity, appointment factory and appointment factory test

--- src/main/java/za/ac/cput/Entity/Appointment.java
+++ src/main/java/za/ac/cput/Entity/Appointment.java
@@ -1,4 +1,119 @@
 package za.ac.cput.Entity;
 
+/* Appointment.java
+   Entity for Appointment
+   Author: Nolusindiso Makosa (219023557)
+   Due Date: 11 June 2021
+*/
+
 public class Appointment {
+    private String appointmentNumber, date, time, reason, patientNumber, doctorNumber, receptionistNumber;
+
+    public Appointment(Builder builder) {
+        this.appointmentNumber = builder.appointmentNumber;
+        this.date = builder.date;
+        this.time = builder.time;
+        this.reason = builder.reason;
+        this.patientNumber = builder.patientNumber;
+        this.doctorNumber = builder.doctorNumber;
+        this.receptionistNumber = builder.receptionistNumber;
+    }
+
+    public String getAppointmentNumber() {
+        return appointmentNumber;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public String getPatientNumber() {
+        return patientNumber;
+    }
+
+    public String getDoctorNumber() {
+        return doctorNumber;
+    }
+
+    public String getReceptionistNumber() {
+        return receptionistNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment{" +
+                "appointmentNumber='" + appointmentNumber + '\'' +
+                ", date='" + date + '\'' +
+                ", time='" + time + '\'' +
+                ", reason='" + reason + '\'' +
+                ", patientNumber='" + patientNumber + '\'' +
+                ", doctorNumber='" + doctorNumber + '\'' +
+                ", receptionistNumber='" + receptionistNumber + '\'' +
+                '}';
+    }
+
+    public static class Builder{
+        private String appointmentNumber;
+        private String date, time, reason, patientNumber, doctorNumber, receptionistNumber;
+
+        public Builder setAppointmentNumber(String appointmentNumber)
+        {
+            this.appointmentNumber = appointmentNumber;
+            return this;
+        }
+        public Builder setDate(String date)
+        {
+            this.date = date;
+            return this;
+        }
+        public Builder setTime(String time)
+        {
+            this.time = time;
+            return this;
+        }
+        public Builder setReason(String reason)
+        {
+            this.reason = reason;
+            return this;
+        }
+        public Builder setPatientNumber(String patientNumber)
+        {
+            this.patientNumber = patientNumber;
+            return this;
+        }
+        public Builder setDoctorNumber(String doctorNumber)
+        {
+            this.doctorNumber = doctorNumber;
+            return this;
+        }
+        public Builder setReceptionistNumber(String receptionistNumber)
+        {
+            this.receptionistNumber = receptionistNumber;
+            return this;
+        }
+
+        //copy method for appointment entity
+        public Builder copy(Appointment appointment){
+            this.appointmentNumber = appointment.appointmentNumber;
+            this.date = appointment.date;
+            this.time = appointment.time;
+            this.reason = appointment.reason;
+
+            return this;
+        }
+        //instance for builder pattern
+        public Appointment build()
+        {
+            return new Appointment(this);
+        }
+    }
+
 }
---
+++ src/test/java/za/ac/cput/Factory/AppointmentFactoryTest.java
@@ -0,0 +1,76 @@
+package za.ac.cput.Factory;
+
+/* AppointmentFactoryTest.java
+   Test class for Appointment Factory
+   Author: Nolusindiso Makosa (219023557)
+   Due Date: 11 June 2021
+*/
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import za.ac.cput.Entity.Appointment;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppointmentFactoryTest {
+   private Appointment appointment, appointment1, appointment2;
+
+        @Test
+        void createAppointment(){
+            Appointment appointment = AppointmentFactory.createAppointment("24 June 2021", "14:30PM",
+                "Consultation", "P01MM", "D01BN", "R01LK");
+
+            Appointment appointment1 = AppointmentFactory.createAppointment("26 June 2021", "15:30PM",
+                    "Fetch results", "P10PS", "D11NM", "R11OP");
+            System.out.println(appointment);
+            System.out.println(appointment1);
+            assertNotNull(appointment);
+       }
+
+       //object equality
+        @Test
+        void objectEquality(){
+        appointment = AppointmentFactory.createAppointment("24 June 2021", "14:30PM",
+                "Consultation", "P01MM", "D01BN", "R01LK");
+        appointment1 = AppointmentFactory.createAppointment("26 June 2021", "15:30PM",
+                "Fetch results", "P10PS", "D11NM", "R11OP");
+        appointment2 = appointment;
+        assertEquals(appointment, appointment2);
+    }
+
+        //object identity
+        @Test
+        void objectIdentity(){
+        appointment = AppointmentFactory.createAppointment("24 June 2021", "14:30PM",
+                "Consultation", "P01MM", "D01BN", "R01LK");
+        appointment1 = AppointmentFactory.createAppointment("26 June 2021", "15:30PM",
+                "Fetch results", "P10PS", "D11NM", "R11OP");
+        assertNotSame(appointment, appointment1);
+    }
+
+         //timeouts
+        @Test
+        @Timeout(1000)
+        void testTimeouts() throws InterruptedException {
+        appointment = AppointmentFactory.createAppointment("24 June 2021", "14:30PM",
+                "Consultation", "P01MM", "D01BN", "R01LK");
+        Thread.sleep(600);
+    }
+
+         //disable test
+        @Disabled ("Disabled until receptionist is up")
+        @Test
+        void testDisable(){
+        appointment = AppointmentFactory.createAppointment("24 June 2021", "14:30PM",
+                "Consultation", "P01MM", "D01BN", "R01LK");
+        appointment1 = AppointmentFactory.createAppointment("26 June 2021", "15:30PM",
+                "Fetch results", "P10PS", "D11NM", "R11OP");
+        appointment2 = appointment;
+            assertEquals(appointment, appointment2);
+    }
+
+
+
+}
