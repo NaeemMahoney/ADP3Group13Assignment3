f7508057ad7030f11ee670fc610952f0169717de
RaeesMan <raees1manuel@gmail.com>
2021-06-11 13:50:00 +0200

    updated code

--- src/test/java/za/ac/cput/Factory/AddressTest.java
+++ src/test/java/za/ac/cput/Factory/AddressTest.java
@@ -7,35 +7,49 @@ Date:11 June 2021
  */
 
 package za.ac.cput.Factory;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import za.ac.cput.Entity.Address;
-import za.ac.cput.Factory.AddressFactory;
 
-import javax.swing.*;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import za.ac.cput.Entity.Address;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AddressTest
 {
-    String addressNumber,houseNumber,streetName,suburb,postalCode;
+    String houseNumber, streetName, suburb, postalCode;
 
+    Address address1 = AddressFactory.build("10", "Albermarle", "hazendal","7764" );
+    Address address2 = AddressFactory.build("73", "Grasmere", "Athlone","7764" );
 
-    @BeforeEach
-    void valuesEntryTest(){
-        addressNumber = JOptionPane.showInputDialog("Enter Address number"," ");
-        houseNumber = JOptionPane.showInputDialog("Enter House number"," ");
-        streetName = JOptionPane.showInputDialog("Enter Street Name"," ");
-        suburb = JOptionPane.showInputDialog("Enter suburb "," ");
-        postalCode = JOptionPane.showInputDialog("Enter postal code"," ");
-
-
-    }
     @Test
-    void test()
+    void testAddress()
     {
-        Address address = AddressFactory.build(addressNumber,houseNumber,streetName,suburb,postalCode);
-        System.out.println(address);
-        assertNotNull(address);
+        Address address1 = AddressFactory.build("10", "Albermarle", "hazendal","7764" );
+        System.out.println(address1);
+        assertNotNull(address1);
+    }
+
+    @Test //Testing equality
+
+    void testEqualityAddress()
+    {
+        assertNotEquals(address1, address2);
+    }
+
+    @Test // testing the timeouts
+    @Timeout(1000)
+    void testTimeoutAddress() throws InterruptedException
+    {
+        Thread.sleep(500);
+        assertNotSame(address1, address2);
+    }
+
+
+    @Test //timeout
+    @Disabled("disabled")
+    void testDisable()
+    {
+        assertNotSame(address1, address2);
     }
 }
