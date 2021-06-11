d86779cb8546ac2393510751c7904097b0d94411
Vanezasito <81571383+Vanezasito@users.noreply.github.com>
2021-06-11 10:37:13 +0200

    Update ReceptionistFactoryTest.java

--- src/test/java/za/ac/cput/Factory/ReceptionistFactoryTest.java
+++ src/test/java/za/ac/cput/Factory/ReceptionistFactoryTest.java
@@ -1,3 +1,9 @@
+/*
+ author : Vaneza Sito 211315044
+ Description: This is a Receptionist Entity.
+ Due Date: 11 June 2021
+ */
+
 package za.ac.cput.Factory;
 
 import org.junit.jupiter.api.BeforeEach;
